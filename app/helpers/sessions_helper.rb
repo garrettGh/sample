bf0ea6c92653de25c2fc61523b0bc96dcfd8854c
garrettGh <garretFbx@gmail.com>
2014-05-12 17:55:19 +0300

    finish user edit, update,index adn destroy without tests

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,6 +17,10 @@ module SessionsHelper
   	@current_user
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   def signed_in?
   	!current_user.nil?
   end
@@ -25,6 +29,15 @@ module SessionsHelper
     current_user.update_attribute(:remember_token, User.encrypt(User.new_remember_token))
     cookies.delete(:remember_token)
     self.current_user = nil
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url if request.get?
   end
 
 end
